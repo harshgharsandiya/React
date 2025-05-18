b7c12f9dda646d8383f249521bc7560936809de0
harshgharsandiya <harshgharsandiya@gmail.com>
2025-05-18 13:20:13 +0530

    counterHook

---
+++ 03CounterHook/README.md
@@ -0,0 +1,5 @@
+- Hooks 
+- To Update All values in UI with the help of Hooks not use so many referces
+
+Assignment 
+value >= 0 and value <=20
---
+++ 03CounterHook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useCallback } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  //Responsible to state change that it will  propagate change in UI (DOM)
+  //variable, function
+  let [counter, setCounter]  = useState(1)
+
+
+  // let counter = 5
+
+  const addValue = () => {
+    // console.log("Value Added", Math.random(), counter);
+    // counter ++;
+    // setCounter(counter);
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    // counter --;
+    // setCounter(counter);
+    setCounter(counter - 1);
+  }
+
+  return (
+   <>
+    <h1>React Counter</h1>
+    <h2>Counter Value: {counter}</h2>
+    
+    <button
+    onClick={addValue}
+    >Increment {counter}</button>
+    <br/>
+    <button
+    onClick={removeValue}
+    >Decrement {counter} </button>
+
+    <p>Footer: {counter}</p>
+
+   </>
+  )
+}
+
+export default App
